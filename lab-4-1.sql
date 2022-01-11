614eefe46f7f0317b2ce8983dcf857e7b41be012
MeenalGakhar <Meenal.gakhar@kellogg.northwestern.edu>
2022-01-11 17:06:24 +0000

    lab 4

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -2,5 +2,9 @@
 
 -- Expected result:
 -- 2935
-
-
+SELECT *FROM players;
+SELECT *FROM players WHERE first_name= "Barry" and last_name= "Nonds";
+SELECT SUM(stats.hits)
+FROM players INNEr JOIN stats ON players.id = stats.player_id
+WHERE players.first_name= "Barry"
+AND players.last_name= "Bonds";
