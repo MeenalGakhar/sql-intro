584cf01a402e1119793169093e69c891507b1402
MeenalGakhar <Meenal.gakhar@kellogg.northwestern.edu>
2022-01-17 19:23:03 +0000

    lab exercises at home

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -5,5 +5,10 @@
 -- +---------------+------------+-----------+-----------+
 -- | New York Mets | Pete       | Alonso    | 53        |
 -- +---------------+------------+-----------+-----------+
-
+SELECT players.first_name, players.last_name, stats.home_runs, teams.name
+FROM stats
+INNER JOIN players ON players.id=stats.player_id
+INNER JOIN teams ON teams.id= stats.team_id
+WHERE teams.year= 2019
+ORDER BY stats.home_runs DESC LIMIT 1;
 
